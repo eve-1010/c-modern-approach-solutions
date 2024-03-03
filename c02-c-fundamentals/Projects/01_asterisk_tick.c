dcd911b44f75889ef34e58470d067a8d18b104d0
eve <eve0110010010@gmail.com>
2024-03-03 13:45:02 +0000

    Start chapter 2 projects

---
+++ c02-c-fundamentals/Projects/01_asterisk_tick.c
@@ -0,0 +1,15 @@
+/* 01_asterisk_tick.c */
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("       *\n");
+    printf("      *\n");
+    printf("     *\n");
+    printf("*   *\n");
+    printf(" * *\n");
+    printf("  *\n");
+
+    return 0;
+}
