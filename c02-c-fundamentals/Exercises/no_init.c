1dc5acd6eb2bfac74ade101e5fe47f2deb8b536d
eve <eve0110010010@gmail.com>
2024-03-03 09:55:12 +0000

    Add no_init.c

---
+++ c02-c-fundamentals/Exercises/no_init.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i, j, k;
+    float x, y, z;
+
+    printf("i: %d, j: %d, k: %d\n", i, j, k);
+    printf("x: %f, y: %f, z: %f\n", x, y, z);
+
+    return 0;
+}
+
+// No pattern to the values
